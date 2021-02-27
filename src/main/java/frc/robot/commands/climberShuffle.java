875f1dcd197b5dc1c4b5ead038e82a63080e7b4d
Aidan Michael Sim <SimAi@student.regisjesuit.com>
2021-02-27 11:16:05 -0700

    fixed the null issue

--- src/main/java/frc/robot/commands/climberShuffle.java
+++ src/main/java/frc/robot/commands/climberShuffle.java
@@ -1,42 +1,42 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+// /*----------------------------------------------------------------------------*/
+// /* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+// /* Open Source Software - may be modified and shared by FRC teams. The code   */
+// /* must be accompanied by the FIRST BSD license file in the root directory of */
+// /* the project.                                                               */
+// /*----------------------------------------------------------------------------*/
 
-package frc.robot.commands;
+// package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Robot;
+// import edu.wpi.first.wpilibj2.command.CommandBase;
+// import frc.robot.Robot;
 
-public class ClimberShuffle extends CommandBase {
-  double m_speed;
-  public ClimberShuffle(double speed) {
-    addRequirements(Robot.m_Climber);
-    m_speed = speed;
-  }
+// public class ClimberShuffle extends CommandBase {
+//   double m_speed;
+//   public ClimberShuffle(double speed) {
+//     addRequirements(Robot.m_Climber);
+//     m_speed = speed;
+//   }
 
-  // Called just before this Command runs the first time
-  @Override
-  public void initialize() {
-  }
+//   // Called just before this Command runs the first time
+//   @Override
+//   public void initialize() {
+//   }
 
-  // Called repeatedly when this Command is scheduled to run
-  @Override
-  public void execute() {
-    Robot.m_Climber.sideClimb(m_speed);
-  }
+//   // Called repeatedly when this Command is scheduled to run
+//   @Override
+//   public void execute() {
+//     Robot.m_Climber.sideClimb(m_speed);
+//   }
 
-  // Make this return true when this Command no longer needs to run execute()
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+//   // Make this return true when this Command no longer needs to run execute()
+//   @Override
+//   public boolean isFinished() {
+//     return false;
+//   }
 
-  // Called once after isFinished returns true
-  @Override
-  public void end(boolean interrupted) {
-    Robot.m_Climber.sideClimb(0);
-  }
-}
+//   // Called once after isFinished returns true
+//   @Override
+//   public void end(boolean interrupted) {
+//     Robot.m_Climber.sideClimb(0);
+//   }
+// }
